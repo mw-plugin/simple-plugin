c0f6b51e8979c2e5fd57171afccfd24f60b1f7f4
midoks <midoks@163.com>
2022-06-20 12:55:10 +0800

    up

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 wget -O /tmp/mw_plugin.zip https://github.com/mw-plugin/simple-plugin/archive/refs/heads/main.zip
-cd /tmp && unzip /tmp/mw_plugin.zip
+cd /tmp && unzip /tmp/mw_plugin.zip 
 
 mkdir -p /www/server/mdserver-web/plugins/simple-plugin
 cp -rf  /tmp/simple-plugin-main/* /www/server/mdserver-web/plugins/simple-plugin
