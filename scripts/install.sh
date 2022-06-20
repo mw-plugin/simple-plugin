9791764aa147ad9c2b6e92a09d44b8786fc8c0a4
midoks <midoks@163.com>
2022-06-20 15:16:56 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,9 @@ cd /tmp && unzip /tmp/mw_plugin.zip
 mkdir -p /www/server/mdserver-web/plugins/simple-plugin
 cp -rf  /tmp/simple-plugin-main/* /www/server/mdserver-web/plugins/simple-plugin
 
+
+
+zip -r -q -o simple-plugin.zip  ./
 # rm -rf /tmp/mw_plugin.zip
 # rm -rf /tmp/simple-plugin-main
 
